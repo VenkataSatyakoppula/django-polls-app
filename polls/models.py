af61d2c8eaa8c4a8bef9b599cf15099ad247a9fc
VenkataSatyakoppula <satya5202@gmail.com>
2024-04-18 20:35:22 -0400

    Initial Commit

---
+++ polls/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    content = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.content
---
+++ polls/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse,JsonResponse
+from .models import Question
+from .serialize import QuestionSerializer 
+
+def list_questions(request):
+    questions = Question.objects.all()
+    q_serialer = QuestionSerializer(questions,many=True)
+    return JsonResponse(q_serialer.data,safe=False)
+
+#def hello_view(request):
+#        return HttpResponse("Hello")
+
